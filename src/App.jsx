8d9cd81fe616983cd2e63b613f5ff74e7217a9ed
SSnuwan00 <srisanjukanuwan@gmail.com>
2025-06-03 12:16:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <header className="bg-blue-500 text-white p-4 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold">Task Manager</h1>
+    </header>
+  )
+}
+
+export default App
