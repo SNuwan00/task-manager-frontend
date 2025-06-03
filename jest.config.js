27e7cda317a6906e064febb73f568acb5b4c6093
SSnuwan00 <srisanjukanuwan@gmail.com>
2025-06-03 22:00:25 +0530

    v3

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,7 @@ export default {
     '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    // Mock CSS imports
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    // Mock file imports
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  collectCoverage: true,
-  coverageReporters: ['text', 'lcov'],
-  coverageDirectory: 'coverage'
-};
+  testMatch: ['**/__tests__/**/*.test.[jt]sx?']
+}
